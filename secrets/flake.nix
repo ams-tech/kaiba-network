a857cefd3dc26e6052b3f8e90599905038e17246
Adam Schafer <ams-tech@users.noreply.github.com>
2026-02-21 16:25:42 -0500

    rough infrastructure set up

--- secrets/flake.nix
+++ secrets/flake.nix
@@ -18,24 +18,8 @@
           pkgs = import nixpkgs { system = system; };
         in
         {
-          packages = {
-            sampleSecretTest = pkgs.callPackage ./test/sampleSecretTest.nix {};
-          };
           checks = {
-            testSecretsAvailability = pkgs.testers.nixosTest {
-              name = "sampleSecretTest";
-              nodes.machine = { config, pkgs, ... }: {
-                imports = [ self.nixosModules.kaibaNetworkSecretsTest ];
-                services.kaibaNetworkSecretsTest.enable = true;
-              };
-
-              # This is a Python "unittest.TestCase", where `t` is the testcase object you can call "asserts" against
-              # https://nixos.org/manual/nixos/stable/#sec-writing-nixos-tests
-              testScript = ''
-                machine.wait_for_unit("default.target")
-                t.assertIn("Linux", machine.succeed("uname"), "Wrong OS")
-              '';
-            };
+            testSecretsAvailability = pkgs.callPackage ./test/sampleSecretTest.nix {};
           };
         }
       );
