da5ed0817d653b965162e76ea490309acafa82ba
Adam Schafer <ams-tech@users.noreply.github.com>
2026-02-24 21:17:00 -0500

    fix node

--- systems/warp.nix
+++ systems/warp.nix
@@ -65,11 +65,6 @@ nixos-raspberrypi.lib.nixosSystem {
                   fsType = "ext4";
                   options = [ "noatime" ];
                 };
-                "/data" = {
-                  device = "/dev/nvmen1p1";
-                  fsType = "ext4";
-                  options = [ "noatime" ];
-                };
               };
             })
           ];
